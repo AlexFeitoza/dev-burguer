25bc44233b8f74d93d364457dcd0eddf14f4cd7c
Alex Feitoza <159736255+AlexFeitoza@users.noreply.github.com>
2024-07-15 04:51:23 -0300

    restart

--- scripts.js
+++ scripts.js
@@ -1,40 +1,15 @@
-
 const list = document.querySelector('ul')
-const buttonShowAll = document.querySelector('.show-all')
-
 let myLi = ''
-function showAll(){
+
 menuOptions.forEach((product) => {
     myLi = myLi +
-            `
-            <li>
-                <img src=${product.src}>
-                <p>${product.name}</p>
-                <p class="item-price">${product.price}</p>
-            </li>
+        `
+        <li>
+            <img src=${product.src}>
+            <p>${product.name}</p>
+            <p class="item-price">R$ ${product.price}</p>
+        </li>
+        `
+})
 
-            `
-            list.innerHTML = myLi
-});
-} 
-
-buttonShowAll.addEventListener('click', showAll)
-
-// Map ------------>
-
-const buttonMapAll = document.querySelector('.map-all')
-
-function mapAllItems(){
-    const newPrices = menuOptions.map((product) => ({
-        name: product.name,
-        price: product.price * 0.9,
-        vegan: product.vegan,
-        src: product.src
-    }))
-
-    console.log(newPrices)
-}
-
-
-
-buttonMapAll.addEventListener('click', 'mapAllItems')
+list.innerHTML = myLi
